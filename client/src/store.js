2f4ffafa86c9f7025ed835e7ae0b0f7d0029df46
Meenakshi <157927822+Panchalmeenakshi17@users.noreply.github.com>
2024-06-02 04:46:10 +0530

    w

--- client/src/store.js
+++ client/src/store.js
@@ -1,27 +1,61 @@
+// import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+// import { thunk } from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import { loginuserReducer, userSignupReducer} from "./reducers/userReducer";
+ 
+// let currentUser = localStorage.getItem('currentDonator') ? JSON.parse(localStorage.getItem('currentUser')) : null
+ 
+
+
+// const rootReducer = combineReducers({
+//  loginuserReducer,
+//  userSignupReducer
+// });
+// const initialState = {
+//   loginuserReducer : {
+//     currentUser
+//   },
+ 
+// };
+
+
+// const middleware = [thunk];
+
+// const store = createStore(rootReducer,initialState,
+//   composeWithDevTools(applyMiddleware(...middleware)),
+//   + window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION());
+// export default store;
+
+
+
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { loginuserReducer, userSignupReducer} from "./reducers/userReducer";
- 
+import {thunk} from "redux-thunk"; // corrected import
+import {loginuserReducer, userSignupReducer} from "./reducers/userReducer";
+
 let currentUser = localStorage.getItem('currentDonator') ? JSON.parse(localStorage.getItem('currentUser')) : null
- 
 
 
 const rootReducer = combineReducers({
- loginuserReducer,
- userSignupReducer
+  loginuserReducer,
+  userSignupReducer,
+  // volunteerRegistration
 });
-const initialState = {
-  loginuserReducer : {
-    currentUser
-  },
- 
-};
 
+const initialState = {
+  loginuserReducer:{
+    currentUser
+  }
+};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer,initialState,
-  composeWithDevTools(applyMiddleware(...middleware)),
-  + window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION());
+// Check if the Redux DevTools Extension is installed, otherwise fallback to a regular compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
+
 export default store;
